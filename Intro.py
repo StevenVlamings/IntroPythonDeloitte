a20b9687fdd4ccc889f5e2fe2b9bbbd461fc123e
StevenVlamings <svlamings@deloitte.com>
2017-04-14 12:02:54 +0200

    added parallelization code

--- Intro.py
+++ Intro.py
@@ -26,3 +26,23 @@ from math import *                  # import all functions from math
 print(sqrt(49))
 # note: might be overlap between your functions and functions from this library & existing function is then overwritten, be careful with this!
 
+from joblib import Parallel, delayed
+import multiprocessing
+import time
+
+def myfunction(i):          # takes a number, prints it, wait 2 seconds & then return the number (simulating a heavy process / calculation)
+    print(i)
+    time.sleep(2)
+    return(i)
+
+if __name__ == '__main__':
+    start = time.time()
+    for i in range(10):
+        myresults = myfunction(i)
+    print("SERIAL", time.time()-start)
+    num_cores = multiprocessing.cpu_count()
+    print("Number of cores used: " + str(num_cores))
+    start=time.time()
+    results = Parallel(n_jobs = num_cores)(delayed(myfunction)(i) for i in range(10))
+    print("PARALLEL", time.time()-start)
+    print(results)
