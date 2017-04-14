e520c96885fc033fbc81b9f19a48d4d807941c6d
StevenVlamings <svlamings@deloitte.com>
2017-04-14 11:33:30 +0200

    imported math library

--- Intro.py
+++ Intro.py
@@ -6,3 +6,23 @@ import pandas as pd
 s = pd.Series([1,2,3])
 print(s)
 
+from MyModule import Animal             # to import the animal class, defined in MyModule.py
+
+zebra = Animal("Marty", 14)
+zebra.description()
+
+def sqrt(number):
+    print("square root of " + str(number))
+sqrt(36)
+
+
+import math
+print(math.sqrt(36))
+
+from math import sqrt               # import square root from math, can quickly call this function
+print(sqrt(36))
+
+from math import *                  # import all functions from math
+print(sqrt(49))
+# note: might be overlap between your functions and functions from this library & existing function is then overwritten, be careful with this!
+
--- MyModule.py
+++ MyModule.py
@@ -1,8 +1,7 @@
 class Animal(object):
-    def _init_(self, startname, age):
+    def __init__(self, startname, age):
         self.name = startname           # attribute 1
         self.age = age                  # attribute 2
     def description(self):              # method 1
         print("this is " + self.name)
         print("he/she is " + str(self.age)+ "years old")
-
