df98f834eff3bc9972120b85bd00c6b861b863db
StevenVlamings <svlamings@deloitte.com>
2017-04-14 11:17:18 +0200

    I've added Animal case to MyModule

---
+++ MyModule.py
@@ -0,0 +1,8 @@
+class Animal(object):
+    def _init_(self, startname, age):
+        self.name = startname           # attribute 1
+        self.age = age                  # attribute 2
+    def description(self):              # method 1
+        print("this is " + self.name)
+        print("he/she is " + str(self.age)+ "years old")
+
